b9d849282a3432bd949f36c48300f7dd4ee5e527
Akosile Tayo <akosilety9@gmail.com>
2021-03-02 11:57:30 +0100

    I removed the span focus of the images

--- Gallery-col/script/gallery-collection.js
+++ Gallery-col/script/gallery-collection.js
@@ -1,18 +1,25 @@
 let allImageCollection = ((async () => {
-    let fetchDataCollectionFromServer = await fetch('https://tboss09.github.io/data/gallery.json');// This fetches the data from github Server hosted on github
+    let fetchDataCollectionFromServer = await fetch('https://tboss09.github.io/data/gallery.json');// This fetches the data from Json file hosted on github
     let data = await fetchDataCollectionFromServer.json();// This parses it and turns it to a Javascript Object making it available to use
     let template = '';// This would output the data into the dom Element
-    let collectionContainer = document.querySelector('.js-collection-container');
+    let collectionContainer = document.querySelector('.js-collection-container'); //Conatiner to Inject the data into
     const idx = Math.floor(Math.random() * data[0]["images"].length); //create random images 
 
+    data.forEach((elem, index) => {// for each item in data element, do this
 
-    data.forEach(elem => {// for each item in data element, do this
         template += `
 <div class="item features-image сol-12 col-md-6 col-lg-3">
                     <div class="item-wrapper">
                         <div class="item-img">
-                            <a href="easter-festive.html?id=${elem.id}"><img src="https://cdn.statically.io/gh/Tboss09/mcttImages/bcc113aa/${elem["images"][idx]}"
-                                    alt="${elem.alt}" data-slide-to="1"></a>
+                            <a href="harvest-day.html?id=${elem.id}">
+                        <img oncontextmenu="return false;" src="https://media.publit.io/file/w_1280/${elem["images"][idx]}" 
+                        srcset="https://media.publit.io/file/w_320/${elem["images"][idx]} 320w,
+                         https://media.publit.io/file/w_640/${elem["images"][idx]} 640w,
+                          https://media.publit.io/file/w_1024/${elem["images"][idx]} 1024w, 
+                          https://media.publit.io/file/w_1280/${elem["images"][idx]} 1280w,
+                           https://media.publit.io/file/w_2048/${elem["images"][idx]} 1400w"
+                            sizes="100vw" alt="img-20191124-142414-9-3120x4160" />
+</a>
                         </div>
                         <div class="item-content">
                             <h5 class="item-title mbr-fonts-style display-7"><strong>${elem.title}</strong></h5>
@@ -24,4 +31,6 @@ let allImageCollection = ((async () => {
                 </div>`
         collectionContainer.innerHTML = template;
     });
+
+
 }))();
--- Gallery-col/script/imageCollection.js
+++ Gallery-col/script/imageCollection.js
@@ -1,14 +1,64 @@
 const id = new URLSearchParams(window.location.search).get('id');
 
 const imageCollection = async () => {
-    const galleryImages = await fetch(`https://tboss09.github.io/data/gallery.json`)
+    const galleryImages = await fetch(`https://tboss09.github.io/data/gallery.json`)  //THis gets the images from the server (github)
+    let res = await galleryImages.json(); // This gets the gallery Images
+    let imageGallery = '';
+    const galleryImagesContainer = document.querySelector('.js-images-container');
+    // console.log(res)
 
-    let res = await galleryImages.json(); // THis gets the gallery Images
-    console.log(res)
+    res.forEach(imgProperties => {
+        if (imgProperties.id == id) {
+            console.log(imgProperties.alt)
 
-    res.forEach(image => {
-        if (image.id == id)
-            console.log(image);
-    });
+            imgProperties["images"].forEach((img, index) => {
+                imageGallery = `<div class="mbr-gallery-item mbr-gallery-item--p4" 
+                data - video - url="false" data - tags="Awesome" >
+                    <div href="#lb-gallery3-13" class="imageContainer" data-slide-to="${index}" data-toggle="modal">
+                    
+                        <img oncontextmenu="return false;" src="https://media.publit.io/file/w_1280/${img}" 
+                        srcset="https://media.publit.io/file/w_320/${img} 320w,
+                         https://media.publit.io/file/w_640/${img} 640w,
+                          https://media.publit.io/file/w_1024/${img} 1024w, 
+                          https://media.publit.io/file/w_1280/${img} 1280w,
+                           https://media.publit.io/file/w_2048/${img} 1400w"
+                            sizes="100vw" alt="img-20191124-142414-9-3120x4160" />
+
+                            </div>
+
+                    </div>`
+
+
+                galleryImagesContainer.innerHTML += imageGallery;
+
+                // console.log(galleryImagesContainer)
+            })
+
+        }
+    })
+
 }
-imageCollection();
+window.addEventListener('load', () => imageCollection())
+
+
+
+$(document).ready(function () {
+    $('.imageContainer').magnificPopup({
+        delegate: 'img',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+
+        }
+    });
+});
+
+
+
